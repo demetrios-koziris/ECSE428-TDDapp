d613ac1be4e29debaa1b606697604a6abd80201c
demetrios-koziris <dwkoziris@gmail.com>
2016-02-17 01:00:44 -0500

    test1 passing

--- src/calculator/PostalRateCalculator.java
+++ src/calculator/PostalRateCalculator.java
@@ -5,7 +5,7 @@ public class PostalRateCalculator {
 	public float length, width, thickness, weight;
 	public Destination dest;
 	public Type type;
-	public Item item;
+	public Payment payment;
 
 	public enum Destination {
 		CANADA, USA, INTERNATIONAL
@@ -13,11 +13,18 @@ public class PostalRateCalculator {
 	public enum Type {
 		LETTER, OTHER
 	}
-	public enum Item {
+	public enum Payment {
 		SINGLE_STAMP, BOOKLET_STAMP, METER_POSTAL_INDICIA
 	}
 
 	public Type determineType() {
+		if (length >= 140 && length < 245 &&
+		    width >= 90 && width < 156 &&
+		    thickness >= 0.18 && thickness < 5 &&
+		    weight >= 2 && weight < 50) 
+		{
+			type = Type.LETTER;
+		}
 		return type;
 	}
 
