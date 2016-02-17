184d15bedb03b3b2fec8532a254b6aa2009140ce
demetrios-koziris <dwkoziris@gmail.com>
2016-02-17 11:55:28 -0500

    test3iia2 passing

--- src/calculator/PostalRateCalculator.java
+++ src/calculator/PostalRateCalculator.java
@@ -68,6 +68,9 @@ public class PostalRateCalculator {
 					if (payment == Payment.STAMP_BOOKLET || payment == Payment.STAMP_SINGLE) {
 						return 1.80f;
 					}
+					else if (payment == Payment.METER_POSTAL_INDICIA) {
+						return 1.71f;
+					}
 				}
 			}
 		}
