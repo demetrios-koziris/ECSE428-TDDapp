47a46683be7f3a16fe1e316163fc83835ccca1c9
Demetrios-Koziris <dwkoziris@gmail.com>
2016-02-18 12:35:40 -0500

    Test5iia2 failing

--- src/test/PostalRateCalculatorTest.java
+++ src/test/PostalRateCalculatorTest.java
@@ -593,5 +593,21 @@ public class PostalRateCalculatorTest {
 		assertEquals(desc1, 5.90f, postal.getPostalRate(), delta);
 	}
 
+
+	@Test
+	public void Test5iia2() {
+
+		PostalRateCalculator postal = new PostalRateCalculator();
+		postal.dest = PostalRateCalculator.Destination.INTERNATIONAL;
+		postal.length = 300;
+		postal.width = 200;
+		postal.thickness = 10;
+		postal.weight = 50;
+		postal.payment = PostalRateCalculator.Payment.METER_POSTAL_INDICIA;
+
+		String desc1 = "Test 5.56 for INTERNATIONAL & OTHER & weight<=100 & METER_POSTAL_INDICIA";
+		assertEquals(desc1, 5.56f, postal.getPostalRate(), delta);
+	}
+	
 }
 
